772a71f2a6735d03bef1ccc1896bade0fcd8feeb
Dieo <dieobox@gmail.com>
2025-08-09 23:58:23 +0700

    Update preview.py

--- facefusion/uis/components/preview.py
+++ facefusion/uis/components/preview.py
@@ -242,7 +242,7 @@ def process_preview_frame(reference_faces : FaceSet, source_face : Face, source_
 	target_vision_frame = restrict_frame(target_vision_frame, (1024, 1024))
 	source_vision_frame = target_vision_frame.copy()
 	if analyse_frame(target_vision_frame):
-		return cv2.GaussianBlur(target_vision_frame, (99, 99), 0)
+		return False
 
 	for processor_module in get_processors_modules(state_manager.get_item('processors')):
 		logger.disable()
